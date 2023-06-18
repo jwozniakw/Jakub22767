d832bae1132a758782b99491ff210e2ced031689
Jakub <wozniakj708@gmail.com>
2023-06-18 19:01:22 +0200

    dodanie kodu product.java oraz utworzenie folderu Repository

---
+++ model/Product.java
@@ -0,0 +1,12 @@
+package com.example.project.model;
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
