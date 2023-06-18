51fc7fa7e2d37ad96d5ce93a7b6dd882f600a32a
Jakub <wozniakj708@gmail.com>
2023-06-18 19:20:31 +0200

    zmiana pliku user

--- model/User.java
+++ model/User.java
@@ -9,4 +9,20 @@ public class User {
         this.age = age;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
